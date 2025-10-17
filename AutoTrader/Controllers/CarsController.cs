15473da7aa48741db69383c306108716a5fc557d
Cs12krisz <csigerkrisztian@gmail.com>
2025-10-17 11:48:33 +0200

    Get all data from cars table succesfully

---
+++ AutoTrader/Controllers/CarsController.cs
@@ -0,0 +1,27 @@
+﻿using AutoTrader.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutoTrader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Car> GetAllRecord() 
+        {
+            using (CarDbContext context = new CarDbContext())
+            {
+                var cars = context.Cars.ToList();
+
+                if (cars != null)
+                {
+                    return Ok(cars);
+                }
+
+                return BadRequest(new { message = "Sikertelen lekérdezés" });
+            }
+            
+        }
+    }
+}
