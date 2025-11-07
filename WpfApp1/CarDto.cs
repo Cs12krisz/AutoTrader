6d7d98e700d3879a07f52b01dc440f94605fd6bd
Cs12krisz <csigerkrisztian@gmail.com>
2025-11-07 09:58:53 +0100

    CarDto a WPF alkalmazásban

---
+++ WpfApp1/CarDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class CarDto
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public DateTime Year { get; set; }
+    }
+}
